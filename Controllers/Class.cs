8a5edf5c76092a6aa5b26654cb9e8ae0c449beec
Gustavo Bacab <Gustavo Bacab@DESKTOP-3G4CMTG>
2026-05-06 23:15:11 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalogo.Controllers
+{
+    public class Class
+    {
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalogo.Models
+{
+    public class Item
+    {
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+    }
+}
